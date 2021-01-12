27d7bd84646087ebef34507ec630fda5c694bfa0
makgalemela <schroodinger@gmail.com>
2021-01-12 19:18:59 +0200

    Intial commit

---
+++ Front-End/src/test/java/com/example/matome/MatomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
