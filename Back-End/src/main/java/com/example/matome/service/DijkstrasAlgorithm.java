ee487accabc01c9bd9deaa0e23594e7ef8abbf8a
makgalemela <schroodinger@gmail.com>
2021-01-24 04:13:35 +0200

    Fixed a for-loop to forEach

--- Back-End/src/main/java/com/example/matome/service/DijkstrasAlgorithm.java
+++ Back-End/src/main/java/com/example/matome/service/DijkstrasAlgorithm.java
@@ -20,20 +20,14 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- *
- *
- *
- *
  * The algorithm used is generic and widely know and what is makes it interesting in this instance is how it is applied
  * to persist the data the graph.
- *
  */
 
 @Component
 public class DijkstrasAlgorithm {
 
     Logger logger = LoggerFactory.getLogger(DijkstrasAlgorithm.class);
-
 
 
     //The graph contain the weight between nodes.
@@ -58,34 +52,32 @@ public class DijkstrasAlgorithm {
 
 
     /**
-     *  We initialise the graph and build it as the adjacency matrix
-     *  We initialise all rows and columns to contains 0s initiali
-     *  Then the relevant indices are populated
-     *  So the worst case time complexity  O(n^2)
-     *
+     * We initialise the graph and build it as the adjacency matrix
+     * We initialise all rows and columns to contains 0s initiali
+     * Then the relevant indices are populated
+     * So the worst case time complexity  O(n^2)
      */
 
 
-    private void init(boolean withDelay){
-            desiredShortestPath = new ArrayList<>();
-            List<PlanetName> planetNameList = planetNameRepository.findAll();
-            int dimensions = planetNameList.size()+1;
+    private void init(boolean withDelay) {
+        desiredShortestPath = new ArrayList<>();
+        List<PlanetName> planetNameList = planetNameRepository.findAll();
+        int dimensions = planetNameList.size() + 1;
 
-            graph = new Double[dimensions][dimensions];
+        graph = new Double[dimensions][dimensions];
 
-            shortestDistances = new Double[dimensions];
+        shortestDistances = new Double[dimensions];
 
-            added = new boolean[dimensions];
+        added = new boolean[dimensions];
 
-            parents = new int[dimensions];
+        parents = new int[dimensions];
 
-            for(int itr = 0 ; itr< dimensions ; itr++){
-                for(int itc = 0 ; itc < dimensions ; itc++){
-                    graph[itr][itc] =  0.0;
+        for (int itr = 0; itr < dimensions; itr++) {
+            for (int itc = 0; itc < dimensions; itc++) {
+                graph[itr][itc] = 0.0;
 
-                }
             }
-
+        }
 
         /**
          *  Build the matrix of doubles because our distance is constructed as double
@@ -95,35 +87,31 @@ public class DijkstrasAlgorithm {
         List<Path> paths = pathRepository.findAll();
         int size = paths.size();
 
-        for(int itr = 0; itr < size ; itr++){
-
-            Path path = paths.get(itr);
-            Double distance = pathRepository.findByOriginAndDestination(path.getOrigin(),path.getDestination())
-                    .getDistance();
-            Double trafficDelay = pathRepository.findByOriginAndDestination(path.getOrigin(),path.getDestination())
-                    .getTrafficDelay();
+        paths.forEach(path -> {
+            Double distance = path.getDistance();
+            Double trafficDelay = path.getTrafficDelay();
 
             /**
              * This build undirected graph.
              * The assumption is that the graph distance between node in longer when there is
              * traffic delay.
              */
-            if(withDelay){
-                graph[path.getOrigin()][path.getDestination()] = distance+trafficDelay;
-                graph[path.getDestination()][path.getOrigin()] = distance+trafficDelay;
-            }else{
+            if (withDelay) {
+                graph[path.getOrigin()][path.getDestination()] = distance + trafficDelay;
+                graph[path.getDestination()][path.getOrigin()] = distance + trafficDelay;
+            } else {
                 graph[path.getDestination()][path.getOrigin()] = distance;
                 graph[path.getOrigin()][path.getDestination()] = distance;
             }
 
-        }
+        });
 
 
         /**
          * Initialise the shortest distances and added
          */
 
-        for(int itr = 0; itr < dimensions ; itr++){
+        for (int itr = 0; itr < dimensions; itr++) {
             shortestDistances[itr] = Double.MAX_VALUE;
             added[itr] = false;
         }
@@ -133,7 +121,7 @@ public class DijkstrasAlgorithm {
     }
 
 
-    private void dijkstra(Integer origin){
+    private void dijkstra(Integer origin) {
 
         shortestDistances[origin] = 0.0;
 
@@ -142,15 +130,13 @@ public class DijkstrasAlgorithm {
 
         int vertices = graph[0].length;
 
-        for (int itr = 1; itr < vertices ; itr++){
+        for (int itr = 1; itr < vertices; itr++) {
 
             int nearestVertex = -1;
             Double shortestDistance = Double.MAX_VALUE;
 
-            for (int itr2 = 0;itr2 < vertices; itr2++)
-            {
-                if (!added[itr2] && shortestDistances[itr2] < shortestDistance)
-                {
+            for (int itr2 = 0; itr2 < vertices; itr2++) {
+                if (!added[itr2] && shortestDistances[itr2] < shortestDistance) {
                     nearestVertex = itr2;
                     shortestDistance = shortestDistances[itr2];
                 }
@@ -160,14 +146,12 @@ public class DijkstrasAlgorithm {
 
             added[nearestVertex] = true;
 
-            for (int itr2 = 0; itr2 < vertices; itr2++)
-            {
+            for (int itr2 = 0; itr2 < vertices; itr2++) {
                 Double edgeDistance = graph[nearestVertex][itr2];
 
                 if (edgeDistance > 0
                         && ((shortestDistance + edgeDistance) <
-                        shortestDistances[itr2]))
-                {
+                        shortestDistances[itr2])) {
                     parents[itr2] = nearestVertex;
                     ///Update the distance from one point to another
                     shortestDistances[itr2] = shortestDistance +
@@ -180,32 +164,34 @@ public class DijkstrasAlgorithm {
     }
 
     /**
+     * Recursive method to compute the shortes distance path
      *
-     *   Recursive method to compute the shortes distance path
      * @param vertex
      * @param parents
      */
-    private void computePath(Integer vertex, int [] parents){
-        if(vertex == NO_PARENT){return;}
-        computePath(parents[vertex],parents);
+    private void computePath(Integer vertex, int[] parents) {
+        if (vertex == NO_PARENT) {
+            return;
+        }
+        computePath(parents[vertex], parents);
         PlanetName planetName = planetNameRepository.findByIndex(vertex);
         desiredShortestPath.add(planetName.getPlanetName());
     }
 
 
     /**
-     *   Utility function to aggregate the methods and compute the results
+     * Utility function to aggregate the methods and compute the results
      *
      * @param req
      * @return
      */
-    public GetShortestPathResponse findShortestPath(GetShortestPathRequest req){
+    public GetShortestPathResponse findShortestPath(GetShortestPathRequest req) {
         PlanetName ori = planetNameRepository.findByPlanetName(req.getOrigin());
         PlanetName des = planetNameRepository.findByPlanetName(req.getDestination());
 
-        if(Objects.isNull(ori) || Objects.isNull(des)){
-            return  null;
-        }else {
+        if (Objects.isNull(ori) || Objects.isNull(des)) {
+            return null;
+        } else {
             GetShortestPathResponse res = new GetShortestPathResponse();
             init(req.getTrafficInfo());
             dijkstra(ori.getIndex());
@@ -214,7 +200,7 @@ public class DijkstrasAlgorithm {
             res.setDestination(des.getPlanetName());
             res.setDistance(String.valueOf(shortestDistances[des.getIndex()]));
             res.setPath(desiredShortestPath);
-            return  res;
+            return res;
 
         }
     }
